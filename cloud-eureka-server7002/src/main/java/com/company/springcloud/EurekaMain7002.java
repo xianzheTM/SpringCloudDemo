d4d7b7054e6205a7be7ffc5fe4410c86d6d36b38
xianzheTM <ylxianzhe@outlook.com>
2020-08-03 12:38:30 +0800

    集群版eureka配置

---
+++ cloud-eureka-server7002/src/main/java/com/company/springcloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package com.company.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xianzheTM
+ * @date 2020/8/3 12:14
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
