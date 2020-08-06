df84991a4b7a0eb4cac9a6a0983465048eca6cb0
xianzheTM <ylxianzhe@outlook.com>
2020-08-06 21:59:13 +0800

    GateWay配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/company/springcloud/GateWayMain9527.java
@@ -0,0 +1,17 @@
+package com.company.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author xianzheTM
+ * @date 2020/8/6 16:07
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
